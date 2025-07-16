cb1f0bd3c106a98e33646522b9eea4b6daf7dd2e
Gabis <Gabis@DESKTOP-I56JV1F>
2025-07-15 22:19:23 -0300

    teste

---
+++ src/main/java/br/edu/ifpr/bsi/trabalhofinal/TrabalhoFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifpr.bsi.trabalhofinal;
+
+/**
+ *
+ * @author Gabis
+ */
+public class TrabalhoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
